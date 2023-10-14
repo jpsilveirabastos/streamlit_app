4a154b6c861c544919b25c41044cfef95bfb81c2
JoaoPedro <jpsilveirabastos@gmail.com>
2023-10-14 19:30:10 -0300

    .

--- core/config.py
+++ core/config.py
@@ -7,6 +7,4 @@ DBNAME = os.getenv('DBNAME')
 USER = os.getenv('USER')
 PASSWORD = os.getenv('PASSWORD')
 HOST = os.getenv('HOST')
-TESTE = os.getenv('TESTE')
 
-print(DBNAME, TESTE)
--- core/db.py
+++ core/db.py
@@ -4,6 +4,7 @@ import psycopg2
 class Db_pg:
     
     def connect():
+        print(DBNAME, USER)
         conn = psycopg2.connect(dbname=DBNAME, user=USER, password=PASSWORD, host=HOST)
         cur = conn.cursor()
         return cur, conn
