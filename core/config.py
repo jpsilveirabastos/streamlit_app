786f811f459bb523848712fa1f221e64303499a6
JoaoPedro <jpsilveirabastos@gmail.com>
2023-10-23 21:02:20 -0300

    .

--- core/config.py
+++ core/config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+TESTE_VAR = os.environ.get("TESTE_VAR")
 DBNAME = os.getenv('DBNAME')
 USER = os.getenv('USER')
 PASSWORD = os.getenv('PASSWORD')
--- core/db.py
+++ core/db.py
@@ -1,10 +1,10 @@
-from .config import DBNAME, USER, PASSWORD, HOST
+from .config import DBNAME, USER, PASSWORD, HOST, TESTE_VAR
 import psycopg2
 
 class Db_pg:
     
     def connect():
-        print(DBNAME, USER)
+        print(TESTE_VAR, DBNAME, USER)
         conn = psycopg2.connect(dbname='st-data', user='st-app', password='Mel4ncia24', host='/cloudsql/streamlit-app-395600:us-central1:st-app')
         cur = conn.cursor()
         return cur, conn
